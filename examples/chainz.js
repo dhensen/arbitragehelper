6a6b46ca7443bddde3b99cc9a3c33db867f69ddc
Dino Hensen <dino.hensen@gmail.com>
2018-01-11 20:55:15 +0100

    feat: refactor and use chain inst. as argument, color output

--- examples/chainz.js
+++ examples/chainz.js
@@ -2,9 +2,13 @@
 
 const arbitrageHelper = require('../index');
 const ccxt = require('ccxt');
+require('ansicolor').nice;
+
 const exchange = new ccxt.binance();
 
 let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+const colorProfit = percentage => percentage > 0 ? `${percentage}`.green : `${percentage}`.red;
 
 const targetAsset = 'ETH';
 arbitrageHelper.findChains(targetAsset, exchange)
@@ -12,8 +16,9 @@ arbitrageHelper.findChains(targetAsset, exchange)
         console.log('Found ' + chains.length + ' symbol chains for ' + targetAsset);
 
         for (let chain of chains) {
-            // console.log(chain);
-            arbitrageHelper.triageForMarkets(exchange, chain)
+            arbitrageHelper.triageForMarkets(exchange, chain).then(chain => {
+                console.log(chain + '; triage: ' + colorProfit(chain.triagePercentage) + ' %');
+            });
 
             // throttle api calls otherwise your IP gets banned
             await exchange.throttle();
